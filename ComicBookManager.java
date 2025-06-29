7b5b17cd18b2e5ffe9c3a6a968394c65c9a9e576
bbails21 <baileyortiz1999@gmail.com>
2025-06-29 19:02:22 +0000

    ComicBookManager.java

--- ComicBookManager.java
+++ ComicBookManager.java
@@ -1,1 +1,26 @@
 
+public class ComicBookManager {
+    public static void main(String[] args) {
+        // Step 1: Create arrays for comic book data
+        String[] titles = {"Spider-Man", "Iron Man", "Captain Marvel"};
+        int[] issues = {1, 2, 3};
+        String[] heroes = {"Peter Parker", "Tony Stark", "Carol Danvers"};
+        int[] powerLevels = {85, 90, 95};
+
+        // Step 2: Print comic book info using a loop
+        for (int i = 0; i < titles.length; i++) {
+            System.out.println("Title: " + titles[i]);
+            System.out.println("Issue #: " + issues[i]);
+            System.out.println("Hero: " + heroes[i]);
+            System.out.println("Power Level: " + powerLevels[i]);
+            System.out.println();
+        }
+
+        // Step 3: Call search method
+        ComicBookUtils.searchComic(titles, heroes,  "Iron Man");
+
+        // Step 4: Call average power method
+        double avg = ComicBookUtils.calculateAverage(powerLevels);
+        System.out.println("Average Power Level: " + avg);
+    }
+}
